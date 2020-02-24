4077827467d90cd640933c270d70c7f204d4970b
xie926 <1187904163@qq.com>
2020-02-24 14:23:45 +0800

    简陋版购物车功能实现

--- react_begining/shop_cart/src/component/simpleCart.js
+++ react_begining/shop_cart/src/component/simpleCart.js
@@ -9,7 +9,7 @@ function Cart (props) {
           <tr key={d.text}>
             <td>{d.text}</td>
             <td>
-              <button onClick={props.addCount(d)}>+</button>
+              <button onClick={() => props.addCount(d)}>+</button>
               {d.count}
               <button>-</button>
             </td>
@@ -22,7 +22,7 @@ function Cart (props) {
     </table>
   )
 }
-class cartSample extends Component {
+class CartSample extends Component {
   constructor(props){
     super(props)
     this.state ={
@@ -37,6 +37,8 @@ class cartSample extends Component {
     }
     this.textChange = this.textChange.bind(this)
     this.addGoods = this.addGoods.bind(this)
+    this.addCount = this.addCount.bind(this)
+    this.addToCart = this.addToCart.bind(this)
   }
   textChange(e){
     console.log(e.target.value)
@@ -59,12 +61,37 @@ class cartSample extends Component {
       history: [...this.state.history, newCart]
     })
   }
+  addToCart(good){
+    // 为了保证视图更新，数组要保证引用地址变化
+    const newCart = [...this.state.cart]
+    // 查询商品对应的购物项是否存在
+    const idx = newCart.findIndex(c => c.text === good.text)
+    // 如果存在则取出
+    const item = newCart[idx]
+
+    if(item){
+      // 更新对象
+      newCart.splice(idx, 1, {...item, count: item.count+1})
+    }else{
+      newCart.push({...good, count: 1})
+    }
+
+    this.setState({
+      cart: newCart,
+      history: [...this.state.history, newCart]
+    })
+  }
+  go(his){
+    this.setState({
+      cart: his
+    })
+  }
   render(){
     const title = this.props.title ? <h1>{this.props.title}</h1> : null
     const goods = this.state.goods.map(good => (
         <li key={good.id}>
           {good.text}
-          <button>加购</button>
+          <button onClick={() => this.addToCart(good)}>加购</button>
         </li>
     ))
     return(
@@ -80,9 +107,15 @@ class cartSample extends Component {
         {/* 购物车列表 */}
         {/* 传递函数用于子组件与父组件进行交换 */}
         <Cart data={this.state.cart} addCount={this.addCount}></Cart>
+
+        {this.state.history.length > 0 ? <p>历史交易记录</p> : null}
+        {this.state.history.length > 0 ? <button key="-1" >0</button> : null}
+        {this.state.history.map((his,i) => (
+          <button key={i} onClick={()=>this.go(his)}>{i + 1}</button>
+        ))}
       </div>
     )
   }
 }
 
-export default cartSample
+export default CartSample
